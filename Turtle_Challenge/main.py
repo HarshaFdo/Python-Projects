f9c5cf6e869e51ee4beaa1a45cc378c2744c8f10
HarshaFdo <fernando.harsha2016@gmail.com>
2025-04-01 01:12:26 +0530

    spinograph and tutle

--- Turtle_Challenge/main.py
+++ Turtle_Challenge/main.py
@@ -1,5 +1,47 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# tim = Turtle()
+#
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.left(90)
 
-timmy_the_turtle = Turtle()
+import turtle as t
+import random
 
-timmy
+tim = t.Turtle()
+t.colormode(255)
+
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
+# colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+
+# for shape_side_n in range(3, 15):
+#     tim.color(random.choice(colours))
+#     draw_shape(shape_side_n)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+directions = [0, 90, 180, 270]
+tim.pensize(12)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
